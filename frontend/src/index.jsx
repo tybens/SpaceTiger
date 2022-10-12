608ed982cea3c624750b0af2d43c64c248661b72
tybens <tb19@princeton.edu>
2022-10-12 17:03:15 -0400

    added universal themeing

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
 import router from "./router";
 
 import store from "./store";
+import theme from "./theme";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </Provider>
   // </React.StrictMode>
 );
---
+++ frontend/src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: blue[500],
+      },
+    },
+  });
+
+  export default theme;
