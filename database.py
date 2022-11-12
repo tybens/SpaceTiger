06fb5d3bf376ab14681dbdbec84b5fc63223671b
evavesely <evesely@princeton.edu>
2022-11-12 17:07:56 -0500

    frontend friendly formatting

--- database.py
+++ database.py
@@ -34,11 +34,25 @@ def get_details(id):
         photos = session.query(models.Photos).filter(models.Photos.space_id == space.id).all()
         amenities = session.query(models.Amenities).filter(models.Amenities.space_id == space.id).all()
 
+        friendly_space = space.to_json()
+        friendly_reviews = []
+        friendly_photos = []
+        friendly_amenities = []
+        
+        for i, review in enumerate(reviews):
+            friendly_reviews.append(review.to_json())
+
+        for i, photo in enumerate(photos):
+            friendly_photos.append(photo.to_json())
+
+        for i, amenity in enumerate(amenities):
+            friendly_amenities.append(amenity.to_json())
+
         details = {
-            'space': space,
-            'reviews': reviews,
-            'photos': photos,
-            'amenities': amenities
+            'space': friendly_space,
+            'reviews': friendly_reviews,
+            'photos': friendly_photos,
+            'amenities': friendly_amenities
         }
     return details
 
@@ -54,6 +68,8 @@ def _test():
     spaces = get_space("Aaron Burr Hall 219")
     for space in spaces:
         print(space)
+    
+    print('')
     
     details = get_details(1)
     for key in details:
