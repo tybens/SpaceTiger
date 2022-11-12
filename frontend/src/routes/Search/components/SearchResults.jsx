9d1f93a201e3b1cbdd541d41b2b0032d0703aec6
Giao Vu Dinh <tvudinh20@gmail.com>
2022-11-12 15:21:23 -0500

    load more

--- frontend/src/routes/Search/components/SearchResults.jsx
+++ frontend/src/routes/Search/components/SearchResults.jsx
@@ -3,17 +3,24 @@ import axios from "axios";
 
 import useStyles from "../styles.js";
 
-// import searchData from "../searchData.json";
 import SpaceItem from "./SpaceItem.jsx";
 
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
+
+const NUM_SHOWN = 20;
 
 export default function SearchResults(props) {
   const { query } = props;
   const [data, setData] = useState([]);
+  const [filtered, setFiltered] = useState([]);
   const [error, setError] = useState(false);
+  const [next, setNext] = useState(NUM_SHOWN);
 
   const classes = useStyles();
+
+  const handleMoreImage = () => {
+    setNext(next + NUM_SHOWN);
+  };
 
   const getData = () => {
     axios
@@ -22,24 +29,25 @@ export default function SearchResults(props) {
         let data = res.data;
         // Setting a data from api
         setData(data.items);
-        // console.log(data.items);
       })
       .catch((err) => console.log(err));
-    // this is obviously temporary, will use fetch later
-    // setData(JSON.parse(JSON.stringify(searchData)));
-    // will also set error here using catch promise
   };
 
-  const renderSpaces = () => {
-    let filtered = data.filter(
-      (space) =>
-        space.name.toLowerCase().includes(query) ||
-        space.location.toLowerCase().includes(query)
+  useEffect(() => {
+    setNext(NUM_SHOWN);
+    setFiltered(
+      data.filter(
+        (space) =>
+          space.name.toLowerCase().includes(query) ||
+          space.location.toLowerCase().includes(query)
+      )
     );
+  }, [data, query]);
 
+  const renderSpaces = () => {
     return (
       <>
-        {filtered.map((space, index) => (
+        {filtered?.slice(0, next)?.map((space, index) => (
           <SpaceItem key={index} space={space} />
         ))}
       </>
@@ -52,9 +60,18 @@ export default function SearchResults(props) {
   }, []);
 
   return (
-    <div className={classes.spaceContainer}>
-      {error && <Typography variant="p">Sorry, an error occured. </Typography>}
-      {renderSpaces()}
-    </div>
+    <>
+      <div className={classes.spaceContainer}>
+        {error && (
+          <Typography variant="p">Sorry, an error occured. </Typography>
+        )}
+        {renderSpaces()}
+      </div>
+      {next < filtered?.length && (
+        <Button className="mt-4" onClick={handleMoreImage}>
+          Load more
+        </Button>
+      )}
+    </>
   );
 }
