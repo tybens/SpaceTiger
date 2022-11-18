923ac40ae77df840f5d2a3ad78f4c673534900ea
Thanya Begum <thanyab15@gmail.com>
2022-11-17 22:05:25 -0500

    Remove Books model and table creation

--- models.py
+++ models.py
@@ -7,14 +7,6 @@ from guid import GUID
 
 Base = sqlalchemy.ext.declarative.declarative_base()
 
-
-class Book(Base):
-    __tablename__ = "books"
-    isbn = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
-    title = sqlalchemy.Column(sqlalchemy.String)
-    quantity = sqlalchemy.Column(sqlalchemy.Integer)
-
-
 class Space(Base):
     __tablename__ = "spaces"
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
