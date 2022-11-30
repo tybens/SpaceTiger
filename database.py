4c794ddc78d0bc49dbd54429f972e1d204d77f0a
tybens <tb19@princeton.edu>
2022-11-30 15:58:43 -0500

    add TA advisor as admin

--- database.py
+++ database.py
@@ -328,7 +328,7 @@ def get_user(puid):
 
 def post_user(puid):
     ret = ""
-    admins = ["tb19", "chenhanz", "evesely", "tbegum", "tgdinh"]
+    admins = ["tb19", "chenhanz", "evesely", "tbegum", "tgdinh", "xl2493"]
     with sqlalchemy.orm.Session(engine) as session:
         query = session.query(models.User).filter(models.User.puid == puid)
         table = query.all()
